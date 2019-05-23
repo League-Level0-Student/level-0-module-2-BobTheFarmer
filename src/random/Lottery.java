ed810f8c161450658840853f1da21edbf9b978f1
BobTheFarmer <league@WTS14-4.attlocal.net>
2019-05-22 18:30:09 -0700

    Lottery IP :smile:

--- src/random/Lottery.java
+++ src/random/Lottery.java
@@ -1,10 +1,20 @@
 package random;
 
+import java.util.Random;
+
+import javax.swing.JOptionPane;
+
 public class Lottery {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		
+		Random rando = new Random();
+		
+		JOptionPane.showMessageDialog(null, "Story Time");
+		JOptionPane.showMessageDialog(null, "This is a true story");
+		JOptionPane.showMessageDialog(null, "ur broke cuz you boght like 400 lottery tickets");
+		JOptionPane.showMessageDialog(null, "You lost them all exepct for one");
+		JOptionPane.showMessageDialog(null, "The numbers on your ticket are, " );
 	}
 
 }
